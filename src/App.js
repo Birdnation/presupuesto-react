36a772ac8a969bef482e55e7198f38d9d4b6656e
Rodrigo Dominguez <rodrigo.dominguez@alumnos.ucn.cl>
2021-06-11 23:06:51 -0400

    feat: add list expense

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Pregunta from "./components/Pregunta";
 import Formulario from "./components/Formulario";
+import Listado from "./components/Listado";
 
 function App() {
   //state para el presupuesto
@@ -29,7 +30,9 @@ function App() {
               <div className="one-half column">
                 <Formulario agregarNuevoGasto={agregarNuevoGasto} />
               </div>
-              <div className="one-half column">2</div>
+              <div className="one-half column">
+                <Listado gastos={gastos} />
+              </div>
             </div>
           )}
         </div>
---
+++ src/components/Gasto.jsx
@@ -0,0 +1,11 @@
+const Gasto = ({ gasto }) => {
+  return (
+    <li className="gastos">
+      <p>
+        {gasto.nombre} <span className="gasto">$ {gasto.cantidad}</span>
+      </p>
+    </li>
+  );
+};
+
+export default Gasto;
